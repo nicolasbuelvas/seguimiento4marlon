427bc5034b103f522769b30ce5f65ac3cab47d63
XxcassianxX <dannastul@gmail.com>
2025-03-31 19:21:40 -0500

    Revision final

--- seguimiento4marlon/src/structure/SumaSubarreglo.java
+++ seguimiento4marlon/src/structure/SumaSubarreglo.java
@@ -3,7 +3,8 @@ package structure;
 public class SumaSubarreglo {
 
     public int[] encontrarSubarreglo(int[] arr, int S) {
-        TablaHash<Integer, Integer> hash = new TablaHash<>();
+
+        TablaHash hash = new TablaHash(arr.length * 2);
         int sumaAcumulada = 0;
 
         for (int i = 0; i < arr.length; i++) {
@@ -13,12 +14,12 @@ public class SumaSubarreglo {
                 return new int[]{0, i};
             }
 
-            Integer indiceAnterior = hash.obtener(sumaAcumulada - S);
+            Integer indiceAnterior = hash.get(sumaAcumulada - S);
             if (indiceAnterior != null) {
                 return new int[]{indiceAnterior + 1, i};
             }
 
-            hash.insertar(sumaAcumulada, i);
+            hash.put(sumaAcumulada, i);
         }
 
         return new int[]{-1, -1};
--- seguimiento4marlon/src/structure/TablaHash.java
+++ seguimiento4marlon/src/structure/TablaHash.java
@@ -1,63 +1,28 @@
 package structure;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+public class TablaHash {
+    private ListaEnlazada[] tabla;
+    private int capacidad;
 
-public class TablaHash<K, V> {
-    private static final int CAPACIDAD_INICIAL = 16;
-    private final List<LinkedList<Entrada<K, V>>> cubetas;
-    private int size;
-
-    public TablaHash() {
-        cubetas = new ArrayList<>(CAPACIDAD_INICIAL);
-        for (int i = 0; i < CAPACIDAD_INICIAL; i++) {
-            cubetas.add(null);
+    public TablaHash(int capacidad) {
+        this.capacidad = capacidad;
+        tabla = new ListaEnlazada[capacidad];
+        for (int i = 0; i < capacidad; i++) {
+            tabla[i] = new ListaEnlazada();
         }
-        size = 0;
     }
 
-    public void insertar(K clave, V valor) {
-        int indice = obtenerIndice(clave);
-        LinkedList<Entrada<K, V>> cubeta = cubetas.get(indice);
-        if (cubeta == null) {
-            cubeta = new LinkedList<>();
-            cubetas.set(indice, cubeta);
-        }
-        for (Entrada<K, V> entrada : cubeta) {
-            if (entrada.clave.equals(clave)) {
-                entrada.valor = valor;
-                return;
-            }
-        }
-        cubeta.add(new Entrada<>(clave, valor));
-        size++;
+    private int hash(int key) {
+        return Math.abs(key) % capacidad;
     }
 
-    public V obtener(K clave) {
-        int indice = obtenerIndice(clave);
-        LinkedList<Entrada<K, V>> cubeta = cubetas.get(indice);
-        if (cubeta != null) {
-            for (Entrada<K, V> entrada : cubeta) {
-                if (entrada.clave.equals(clave)) {
-                    return entrada.valor;
-                }
-            }
-        }
-        return null;
+    public void put(int key, int value) {
+        int indice = hash(key);
+        tabla[indice].agregar(key, value);
     }
 
-    private int obtenerIndice(K clave) {
-        return Math.abs(clave.hashCode() % cubetas.size());
-    }
-
-    private static class Entrada<K, V> {
-        K clave;
-        V valor;
-
-        Entrada(K clave, V valor) {
-            this.clave = clave;
-            this.valor = valor;
-        }
+    public Integer get(int key) {
+        int indice = hash(key);
+        return tabla[indice].obtenerValor(key);
     }
 }
